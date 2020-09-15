297ac7bd2aeb538e5a918c58a09a386a0e9cb15d
lixiliang <3151596778@qq.com>
2020-09-15 17:14:21 +0800

    测试上传项目

---
+++ demo01/src/main/java/cn/kgc/ssm/App.java
@@ -0,0 +1,14 @@
+package cn.kgc.ssm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("测试demo01");
+        System.out.println( "Hello World!" );
+    }
+}

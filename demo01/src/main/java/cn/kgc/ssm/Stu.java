38eefe93ad6ebca6e4e62f8c76fc6e851e7ff305
shiyi <1135537585@qq.com>
2020-09-19 09:19:31 +0800

    添加stu

---
+++ demo01/src/main/java/cn/kgc/ssm/Stu.java
@@ -0,0 +1,40 @@
+package cn.kgc.ssm;
+
+public class Stu {
+    private  int id;
+    private String name;
+    private int age;
+
+    public Stu() {
+    }
+
+    public Stu(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}

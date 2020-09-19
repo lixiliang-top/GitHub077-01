068a97363f87f0a4ce51c39ad3e48f7a80204147
lixiliang <3151596778@qq.com>
2020-09-19 09:45:05 +0800

    添加一个小猫的实体类

---
+++ demo01/src/main/java/cn/kgc/ssm/cat.java
@@ -0,0 +1,26 @@
+package cn.kgc.ssm;
+
+/**
+ * @author 李锡良
+ * @create 2020-09-19 9:44
+ */
+public class cat {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
